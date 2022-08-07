7eb5aa78891d9e72df1284fd0f6c2c4b5946dfe5
Yulia <yulia.v@Yulias-MacBook.local>
2022-08-07 23:30:56 +0700

    Run server; connect sequelize

---
+++ server/index.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const sequelize = require('./db');
+const model = require('./models/model');
+const route = require('./routes');
+const errorHandler = require('./middlewares/handleErrorMiddleware');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const PORT = process.env.PORT || 5000;
+
+app.use('/api', route);
+app.use(errorHandler);
+
+const start = async () => {
+  try {
+    await sequelize.authenticate();
+    await sequelize.sync();
+    app.listen(PORT, () => console.log(`Server is started on port ${PORT}`));
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+start();
